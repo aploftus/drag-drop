c5f39c332bf2ce68d17264ed18df677506e7a871
aploftus <aploftus@users.noreply.github.com>
2018-01-28 21:16:12 -0800

    Reorder divs in column with react-reorder

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Reorder, {
+  reorder,
+  reorderImmutable,
+  reorderFromTo,
+  reorderFromToImmutable
+} from 'react-reorder';
 
 import './main.css';
 
 import Add from './components/add.jsx';
-import List from './components/list.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,26 +17,49 @@ class App extends React.Component {
 
     this.state = {
       items: ['x', '2x', '3x'],
-      selectedIndex: null
     };
 
     this.makeNewDiv = this.makeNewDiv.bind(this);
-    this.selectItem = this.selectItem.bind(this);
+    this.onReorder = this.onReorder.bind(this);
   }
 
   makeNewDiv() {
     this.setState( {items: this.state.items.concat('')} );
   }
 
-  selectItem(index) {
-    this.setState({ selectedIndex: index });
+  onReorder (event, previousIndex, nextIndex, fromId, toId) {
+    this.setState({
+      items: reorder(this.state.items, previousIndex, nextIndex)
+    });
   }
 
   render() {
     return (
       <div>
         <Add makeNewDiv={this.makeNewDiv} />
-        <List items={this.state.items} selectedIndex={this.state.selectedIndex} selectItem={this.selectItem} />
+        <Reorder
+          reorderId="div-list" // Unique ID that is used internally to track this list (required)
+          placeholderClassName="placeholder" // Class name to be applied to placeholder elements (optional), defaults to 'placeholder'
+          draggedClassName="dragged" // Class name to be applied to dragged elements (optional), defaults to 'dragged'
+          lock="horizontal" // Lock the dragging direction (optional): vertical, horizontal (do not use with groups)
+          onReorder={this.onReorder} // Callback when an item is dropped (you will need this to update your state)
+          placeholder={
+            <div className="custom-placeholder" /> // Custom placeholder element (optional), defaults to clone of dragged element
+          }
+        >
+          {this.state.items.map((item, index) => {
+            return (
+              <div className="item" key={index}>
+                <div className="reorder">
+                  <span className="index">{index + 1}</span>
+                </div>
+                <span>{item}</span>
+                <span className="remove">x</span>
+              </div>
+            );
+          }
+          )}
+        </Reorder>
       </div>
     );
   }
